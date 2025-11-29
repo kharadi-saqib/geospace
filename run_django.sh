652d85de00a465934860173d2de545c6b6ba9186
kharadi-saqib <kharadisaqib@gmail.com>
2025-11-29 12:15:39 +0530

    updated shell file

--- run_django.sh
+++ run_django.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Activate virtual environment
-source /home/administrator/Geo_node_08SEP_25/geonode/venv/bin/activate
+source /home/ubuntu/geospace/venv/bin/activate
 
 # Ensure the script is executed from the correct directory
-cd /home/administrator/Geo_node_08SEP_25/geonode
+cd /home/ubuntu/geospace
 
 # Start Django server in foreground
-exec python manage.py runserver 209.182.234.193:8007
+python3 manage.py runserver 0.0.0.0:8000
